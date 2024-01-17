3ecffb1e38ccfd596a22efbbb83cdb7fe564ee32
Tóth Tibor <toth.tibor@autsoft.hu>
2024-01-17 19:49:42 +0100

    VM recipe groups

--- MvvmLab/ViewModels/MainViewModel.cs
+++ MvvmLab/ViewModels/MainViewModel.cs
@@ -1,10 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
+using MvvmLab.Contracts.ViewModels;
+using MvvmLab.Core.Models;
+using MvvmLab.Core.Services;
+
 namespace MvvmLab.ViewModels;
 
-public partial class MainViewModel : ObservableRecipient
+public partial class MainViewModel : ObservableRecipient, INavigationAware
 {
-    public MainViewModel()
+    private readonly IRecipeService _recipeService;
+
+    [ObservableProperty]
+    private List<RecipeGroup>? _recipeGroups = new();
+
+    public MainViewModel(IRecipeService recipeService)
+    {
+        _recipeService = recipeService;
+    }
+
+    public async void OnNavigatedTo(object parameter)
+    {
+        RecipeGroups = await _recipeService.GetRecipeGroupsAsync();
+    }
+
+    public void OnNavigatedFrom()
     {
     }
 }
