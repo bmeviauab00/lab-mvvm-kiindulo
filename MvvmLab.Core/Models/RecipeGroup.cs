ab405d110edc3f40297ef02fc0306b468fae8489
Tóth Tibor <toth.tibor@autsoft.hu>
2024-01-17 19:33:39 +0100

    DTOs

---
+++ MvvmLab.Core/Models/RecipeGroup.cs
@@ -0,0 +1,8 @@
+﻿namespace MvvmLab.Core.Models;
+
+public class RecipeGroup
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public List<RecipeHeader> Recipes { get; set; }
+}
---
+++ MvvmLab.Core/Models/RecipeHeader.cs
@@ -0,0 +1,9 @@
+﻿namespace MvvmLab.Core.Models;
+
+public class RecipeHeader
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string BackgroundImage { get; set; }
+    public string TileImage { get; set; }
+}
