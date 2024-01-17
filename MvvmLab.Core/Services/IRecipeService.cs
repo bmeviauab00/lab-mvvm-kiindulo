c1018d6bf3045e43d595d09cfa203f18af366f54
Tóth Tibor <toth.tibor@autsoft.hu>
2024-01-17 19:38:22 +0100

    recipe service recipe groups

---
+++ MvvmLab.Core/Services/IRecipeService.cs
@@ -0,0 +1,8 @@
+﻿using MvvmLab.Core.Models;
+
+namespace MvvmLab.Core.Services;
+
+public interface IRecipeService
+{
+    public Task<List<RecipeGroup>> GetRecipeGroupsAsync();
+}
---
+++ MvvmLab.Core/Services/RecipeService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Json;
+
+using MvvmLab.Core.Models;
+
+namespace MvvmLab.Core.Services;
+
+public class RecipeService : IRecipeService
+{
+    private readonly string _baseUrl = "https://bmecookbook.azurewebsites.net/api";
+
+    public async Task<List<RecipeGroup>> GetRecipeGroupsAsync()
+    {
+        using var client = new HttpClient();
+        return await client.GetFromJsonAsync<List<RecipeGroup>>($"{_baseUrl}/Recipes/Groups");
+    }
+}
