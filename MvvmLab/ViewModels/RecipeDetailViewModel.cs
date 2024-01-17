f5f3781ce78356b203c74326c3c304117f47ca40
Tóth Tibor <toth.tibor@autsoft.hu>
2024-01-17 20:19:36 +0100

    details vm

---
+++ MvvmLab/ViewModels/RecipeDetailViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using MvvmLab.Contracts.ViewModels;
+using MvvmLab.Core.Models;
+using MvvmLab.Core.Services;
+
+namespace MvvmLab.ViewModels;
+
+public partial class RecipeDetailViewModel : ObservableRecipient, INavigationAware
+{
+    private readonly IRecipeService _recipeService;
+
+    public RecipeDetailViewModel(IRecipeService recipeService)
+    {
+        _recipeService = recipeService;
+    }
+
+    [ObservableProperty]
+    private Recipe? _recipe;
+
+    public async void OnNavigatedTo(object parameter)
+    {
+        Recipe = await _recipeService.GetRecipeAsync((int)parameter);
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+}
+
